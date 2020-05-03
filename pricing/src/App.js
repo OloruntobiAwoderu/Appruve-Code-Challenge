62c761df9d2da5421598b2b93ee950b384e62379
oloruntobi <awoderuoloruntobi@gmail.com>
2020-05-03 18:03:44 +0100

    Create-react-app

---
+++ pricing/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Pricing from './component/pricing';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Pricing />
+    </div>
+  );
+}
+
+export default App;
