a1ad7d27435d92e07d235e1256eacbbdbf8b46e1
oloruntobi <awoderuoloruntobi@gmail.com>
2020-05-03 18:31:43 +0100

    Made modifications to pricing

--- pricing/src/component/pricing.js
+++ pricing/src/component/pricing.js
@@ -1,114 +1,124 @@
-
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import StarIcon from '@material-ui/icons/StarBorder';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import StarIcon from "@material-ui/icons/StarBorder";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://www.appruve.co/">
         Appruve.co
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  '@global': {
+const useStyles = makeStyles(theme => ({
+  "@global": {
     ul: {
       margin: 0,
       padding: 0,
-      listStyle: 'none',
+      listStyle: "none"
     },
+    h1: {
+        marginRight: "85%",
+        color: "#9AD9BE"
+    }
   },
+
   appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
+    borderBottom: `1px solid ${theme.palette.divider}`
   },
   toolbar: {
-    flexWrap: 'wrap',
+    flexWrap: "wrap"
   },
   toolbarTitle: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   link: {
-    margin: theme.spacing(1, 1.5),
+    margin: theme.spacing(1, 1.5)
   },
   heroContent: {
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(8, 0, 6)
   },
   cardHeader: {
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700]
   },
   cardPricing: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
-    marginBottom: theme.spacing(2),
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2)
   },
   footer: {
     borderTop: `1px solid ${theme.palette.divider}`,
     marginTop: theme.spacing(8),
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       paddingTop: theme.spacing(6),
-      paddingBottom: theme.spacing(6),
-    },
-  },
+      paddingBottom: theme.spacing(6)
+    }
+  }
 }));
 
 const tiers = [
   {
-    title: 'Bronze',
-    price: '10',
-    description: ['5000 API calls', 'Up to 1000 ID Verifications', 'Help center access', 'Email support'],
-    buttonText: 'Sign up for free',
-    buttonVariant: 'outlined',
+    title: "Bronze",
+    price: "10",
+    description: [
+      "5000 API calls",
+      "Up to 1000 ID Verifications",
+      "Help center access",
+      "Email support"
+    ],
+    buttonText: "Sign up",
+    buttonVariant: "outlined"
   },
   {
-    title: 'Gold',
-    subheader: 'Most popular',
-    price: '50',
+    title: "Gold",
+    subheader: "Most popular",
+    price: "50",
     description: [
-      'Unlimited API calls',
-      'Up to 100000 ID verificatons',
-      'Help center access',
-      'Priority email support',
+      "Unlimited API calls",
+      "Up to 100000 ID verificatons",
+      "Help center access",
+      "Priority email support"
     ],
-    buttonText: 'Get started',
-    buttonVariant: 'contained',
+    buttonText: "Get started",
+    buttonVariant: "contained"
   },
   {
-    title: 'Silver',
-    price: '30',
+    title: "Silver",
+    price: "30",
     description: [
-      '30000000 API calls',
-      'Up to 50000 ID verifications',
-      'Help center access',
-      'Phone & email support',
+      "30000000 API calls",
+      "Up to 50000 ID verifications",
+      "Help center access",
+      "Phone & email support"
     ],
-    buttonText: 'Contact us',
-    buttonVariant: 'outlined',
-  },
+    buttonText: "Sign up",
+    buttonVariant: "outlined"
+  }
 ];
-
 
 export default function Pricing() {
   const classes = useStyles();
@@ -116,37 +126,55 @@ export default function Pricing() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        className={classes.appBar}
+      >
         <Toolbar className={classes.toolbar}>
-          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-            Appruve
-          </Typography>
-          
-          <Button href="#" color="primary" variant="outlined" className={classes.link}>
+          <h1>Appruve</h1>
+
+          <Button
+            href="https://dashboard.appruve.co/login"
+            color="primary"
+            variant="outlined"
+            className={classes.link}
+          >
             Login
           </Button>
         </Toolbar>
       </AppBar>
       {/* Hero unit */}
       <Container maxWidth="sm" component="main" className={classes.heroContent}>
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
           Pricing
         </Typography>
-        
       </Container>
       {/* End hero unit */}
       <Container maxWidth="md" component="main">
         <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-        
-            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
+          {tiers.map(tier => (
+            <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === "Enterprise" ? 12 : 6}
+              md={4}
+            >
               <Card>
                 <CardHeader
                   title={tier.title}
                   subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                  action={tier.title === "Pro" ? <StarIcon /> : null}
                   className={classes.cardHeader}
                 />
                 <CardContent>
@@ -159,15 +187,25 @@ export default function Pricing() {
                     </Typography>
                   </div>
                   <ul>
-                    {tier.description.map((line) => (
-                      <Typography component="li" variant="subtitle1" align="center" key={line}>
+                    {tier.description.map(line => (
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        key={line}
+                      >
                         {line}
                       </Typography>
                     ))}
                   </ul>
                 </CardContent>
                 <CardActions>
-                  <Button fullWidth variant={tier.buttonVariant} color="primary">
+                  <Button 
+                    fullWidth
+                    variant={tier.buttonVariant}
+                    color="primary"
+                    href="https://dashboard.appruve.co/register"
+                  >
                     {tier.buttonText}
                   </Button>
                 </CardActions>
@@ -178,7 +216,6 @@ export default function Pricing() {
       </Container>
       {/* Footer */}
       <Container maxWidth="md" component="footer" className={classes.footer}>
-    
         <Box mt={5}>
           <Copyright />
         </Box>
